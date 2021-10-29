3a67974b3716cad7ca8f98c87f00bc471ab1747d
Jorne <mr.jorne@gmail.com>
2021-10-29 12:40:01 +0200

    Bijna
    Todo: welke hook te gebruiken?

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
 })
-export class ListPage implements OnInit {
+export class ListPage implements AfterViewChecked, AfterViewInit {
 
   list={
     yellow:'full',
@@ -14,9 +14,51 @@ export class ListPage implements OnInit {
     green:'small'
   }
 
-  constructor() { }
+  containerHeight :number =0;
+  usedHeight: number=0;
+  maxHeight: number = 350;
+  redPaneContentHeight;
 
-  ngOnInit() {
+  checkedHeights: boolean = false;
+
+  @ViewChild('paneslist') panesList: ElementRef;
+  @ViewChild('yellowpane') yellowPane: ElementRef;
+  @ViewChild('bluepane') bluePane: ElementRef;
+  @ViewChild('redpane') redPane: ElementRef;
+  @ViewChild('greenpane') greenPane: ElementRef;
+  root = document.documentElement;
+
+  constructor() {
+    this.root.style.setProperty('--max', this.maxHeight+"px")
+  }
+
+  ngAfterViewChecked(): void {
+   this.getHeights();
+  }
+  ngAfterViewInit(): void {
+  //  this.getHeights();
+  }
+
+  test(){
+    console.log(this.maxHeight);
+  }
+
+  getHeights(){
+    console.log('get heights')
+    this.containerHeight  =  this.panesList.nativeElement.offsetHeight;
+    this.usedHeight       =  this.yellowPane.nativeElement.offsetHeight
+                          + this.bluePane.nativeElement.offsetHeight
+                          + (this.greenPane.nativeElement.offsetHeight/1.5);
+
+    this.calculatedMaxHeight(this.containerHeight, this.usedHeight);
+  }
+
+  calculatedMaxHeight(containerHeight, usedHeight){
+    this.maxHeight = (containerHeight - usedHeight);
+    this.root.style.setProperty('--max', this.maxHeight+'px')
+
+    this.checkedHeights=true;
+
   }
 
   onToggleSize(name:string, size: string){
